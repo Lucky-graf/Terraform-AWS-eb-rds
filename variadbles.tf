8fbf5f14fb391f7c54d516b69e80bcb61a4a8523
Bodya <57416546+Lucky-graf@users.noreply.github.com>
2022-02-21 11:59:31 +0200

    Update variadbles.tf

--- variadbles.tf
+++ variadbles.tf
@@ -11,7 +11,7 @@ variable "data_base" {
   type = map(string)
   sensitive = true
   default = {
-    host      = "tf-database-1.cwyboycueldk.us-east-2.rds.amazonaws.com"
+    host      = ${data.aws_db_instance.database.address}
     base      = "web2"
     user      = "myuser"
     password  = "mypassword"
